45aaabee1a525e575450b5adee104361ee9a182a
Andrew Doyle <andrew.doyle.83@gmail.com>
2017-07-06 20:07:26 +1000

    reading md file

--- src/blog/some-blog-post.md
+++ src/blog/some-blog-post.md
@@ -1,11 +1,10 @@
-
 # Some Blog Post
 
 This would be a blog Post.
 
 ### Here is some code
 
-```
+```javascript
 function helloWorld() {
   alert('Hello World');
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactMarkdown from 'react-markdown';
 import someBlogPost from '../../blog/some-blog-post.md'
 
-export const HomePage = () => {
-  return(
-    <div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>  
-      <h3>Blog</h3>
-      <ReactMarkdown source={someBlogPost} />
-    </div>    
-  );  
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {    
+    fetch(someBlogPost)
+    .then(data => {
+      return data.text()
+    })
+    .then(text => {      
+      this.setState({
+        markdown: text
+      });
+    })
+  }
+  
+  render() {  
+    const {markdown} = this.state;
+    console.log('STATE', this.state)
+    return(
+      <div>
+        <p className="App-intro test">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>  
+        <h3>Blog</h3>        
+        <ReactMarkdown source={markdown} />
+      </div>    
+    );  
+  }
 };
 
 export default HomePage;
