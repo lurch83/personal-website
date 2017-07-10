c783d29af0ea5806da3a4a07bc1e9ac35cb2f3c0
Andrew Doyle <andrew.doyle.83@gmail.com>
2017-07-10 23:04:28 +1000

    refactored and working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {HashRouter, Route, Link} from 'react-router-dom';
 import './App.css';
 import styled from 'styled-components';
 import Box from '../Box/Box';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import About from '../About/About';
 
+//TODO: Move this Container
 const Container = styled.div`
   width: 100%;
   max-width: 75rem;
@@ -16,12 +19,13 @@ const Container = styled.div`
 class App extends Component {
   render() {
     return (         
-        <div className="App">
-          <Container>
-            <Header />                     
-            <Main />
-          </Container>
-        </div>
+      <HashRouter>
+        <Container>
+          <Header />
+          <Route exact path="/" component={Main} />
+          <Route path="/about" component={About} />
+        </Container>
+      </HashRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Link} from 'react-router-dom';
 import App from './components/App/App';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import About from './components/About/About.jsx';
 import './index.css';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  width: 100%;
-  max-width: 75rem;
-  margin-right: auto;
-  margin-left: auto;
-  box-sizing: border-box;  
-`;
-
 
 ReactDOM.render(  
-  <HashRouter>
-    <Container>
-      <Header />
-      <Route exact path="/" component={Main} />
-      <Route path="/about" component={About} />
-    </Container>
-  </HashRouter>
+  <App />
   , document.getElementById('root')
 );
