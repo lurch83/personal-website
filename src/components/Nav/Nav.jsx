8d42bcdd9cde1fbb3096e000d2794efb2a462fd0
Andrew Doyle <andrew.doyle.83@gmail.com>
2017-10-05 18:47:10 +1100

    mobile nav is still not 100% working

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {NavBox, NavContainer, Button} from './NavComponents';
+import {stack as Menu} from 'react-burger-menu';
 
 const NavLink = props => {
 
@@ -9,9 +10,9 @@ const NavLink = props => {
   const isSelected = (pathname === to);
   
   return(
-    <Button selected={isSelected}>
+    // <Button selected={isSelected}>
       <Link to={to}>{title}</Link>
-    </Button>
+    // </Button>
     
   );
 };
@@ -23,38 +24,75 @@ NavLink.PropTypes = {
 };
 
 const MobileNav = styled.div`
-  background-color: green;
-  display: none;
-  @media(max-width: 768px){
-    display: block;
-  }
+ 
 `;
 
 
 class Nav extends Component {
+  constructor(props){
+    super(props);    
+    this.state = {menuOpen: false};
+  }
   
+  handleMenuClick(event){    
+    this.setState({menuOpen: false})
+  }
+
   render() {  
     const {pathname} = this.props.location;
+    const {menuOpen} = this.state; 
+    let styles = {
+        bmBurgerButton: {
+            position: 'fixed',
+            width: '36px',
+            height: '30px',
+            left: '36px',
+            top: '36px',
+            color: 'red'
+        },
+        bmBurgerBars: {
+            background: '#373a47'
+        },
+        bmCrossButton: {
+            height: '24px',
+            width: '24px'
+        },
+        bmCross: {
+            background: '#bdc3c7'
+        },
+        bmMenu: {
+            background: '#373a47',
+            padding: '2.5em 1.5em 0',
+            fontSize: '1.15em',
+            overflow: 'hidden'          
+        },        
+        bmMorphShape: {
+            fill: '#373a47'
+        },
+        bmItemList: {
+            color: '#b8b7ad',
+            padding: '0.8em',
+            a: {              
+              color: 'blanchedalmond'        
+            }
+        },
+        bmOverlay: {
+            background: 'rgba(0, 0, 0, 0.3)'
+        }
+        
+    };
 
     return(    
-      <div>      
-        <NavBox className="nav-topnav">
-          <NavContainer>
-            <NavLink 
-              pathname={pathname}
-              to="/" 
-              title="Home" 
-              />
-            <NavLink 
-              pathname={pathname}
-              to="/about" 
-              title="About Me" 
-              />   
-          </NavContainer>
-        </NavBox>
-        <MobileNav className="nav-mobilenav">
-          Mobile Nav
-        </MobileNav>
+        
+      <div>
+             
+          <Menu isOpen={menuOpen} styles={styles}>
+          <div>open: {menuOpen}</div>   
+            <Link onClick={ this.handleMenuClick } id="home"  to="/">Home</Link>
+            <Link onClick={ this.handleMenuClick } id="about" to="/about">About</Link>
+            {/*<a id="contact" className="menu-item" href="/contact">Contact</a>
+            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>*/}
+          </Menu>
       </div>
     );    
   }
